ad04c9399fd4e62df3c2bfae62ccdc7ec2005d84
Felipe Couto <felipecoutodarocha@gmail.com>
2022-06-17 16:08:58 -0300

    Salvano projeto.

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburguer = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav__list');
+const navLinks = document.querySelectorAll('.nav__list-item--link');
+
+hamburguer.onclick = () => {
+  navList.classList.toggle('active');
+  hamburguer.classList.toggle('active');
+};
+
+const removeActive = (links) => {
+  links.forEach((link) => {
+    link.classList.remove('active');
+  });
+};
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    removeActive(navLinks);
+    link.classList.add('active');
+    navList.classList.remove('active');
+  });
+});
+
